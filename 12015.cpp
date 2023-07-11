e42006394f811e1998c9e70c797c8b64640ddb26
junoooooooooo <wnsgh732@gmail.com>
2023-07-11 18:04:56 +0900

    Update 12015.cpp

--- 12015.cpp
+++ 12015.cpp
@@ -1,27 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[500005];
+vector<int> v[500005];
+int vis[500005];
 void solve() {
-  int n;
-  cin >> n;
-  map<int, int> mp;
-  int ans = 0;
-  vector<int> v(n+1, 0);
-  int vi = 1;
-  for(int i = 0; i < n; i ++) {
-    int t;
-    cin >> t;
-    if(i == 0) v[i] = t;
-    else {
-      auto l = lower_bound(v.begin(), v.begin() + vi, t);
-      *l = t;
-      if(l == v.begin() + vi) vi ++;
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    for(int i = 1; i <= n; i ++) {
+      cin >> a[i];
     }
-  }
-  // for(int i = 0; i < vi; i ++) {
-  //   cout << v[i] << ' ';
-  // }
-  cout << vi;
+    for(int i = 0; i < m; i ++) {
+      int t1, t2;
+      cin >> t1 >> t2;
+      v[t1].push_back(t2);
+      v[t2].push_back(t1);
+    }
+    queue<int> q;
+    for(int i = 0; i < y; i ++) {
+      int t;
+      cin >> t;
+      q.push(t);
+      vis[t] = 1;
+      while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for(auto c : v[cur]) {
+          if(vis[c] != 0 && vis[c] <= vis[cur] + 1) continue; 
+          vis[c] = vis[cur] + 1;
+          q.push(c);
+        }
+      }
+    }
+    vector<long long> val;
+    for(int i = 1; i <= n; i ++) {
+      val.push_back(1ll * (vis[i]-1) * a[i]);
+    }
+    sort(val.begin(), val.end());
+    // for(int i = 0; i < n; i ++) {
+    //   cout << val[i] << ' ';
+    // }
+    if(val[0] < 0) cout << -1;
+    else {
+      long long ans = 0;
+      for(int i = 0; i < x; i ++) {
+        ans += val[val.size() - 1 - i];
+      }
+      cout << ans;
+    }
 }
 
 // ************************************
