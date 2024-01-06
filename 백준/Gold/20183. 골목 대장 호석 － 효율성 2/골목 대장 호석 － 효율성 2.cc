7dcaa0e3f1ab8d83e9916ca5339051b0fbe79821
juno <94045552+junoooooooooo@users.noreply.github.com>
2024-01-07 01:26:44 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 2148 ms, Memory: 34996 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -17,39 +17,46 @@ typedef vector<ll> vl;
 const char nl = '\n';
 const ll mx = 1e14+5;
 
-vector<pi> adj[100005];
+vector<pl> adj[100'000+5];
+vector<ll> d(100005);
+priority_queue<pl, vector<pl>, greater<pl>> pq;
+
+ll n, m, a, b, c;
+
+bool check(ll mid) {
+  fill(all(d), mx);
+  pq.push({0, a});
+  d[a] = 0;
+  while(!pq.empty()) {
+    auto cur = pq.top();
+    pq.pop();
+    if(d[cur.Y] != cur.X) continue; 
+    for(auto nxt : adj[cur.Y]) {
+      if(nxt.X > mid) continue;
+      ll t = nxt.X + cur.X;
+      if(t < d[nxt.Y]) {
+        d[nxt.Y] = t;
+        pq.push({t, nxt.Y});
+      }
+    }
+  }
+  if(d[b] == mx || d[b] > c) return 1;
+  return 0;
+}
 void solve() {
-  int n, m, a, b, c;
   cin >> n >> m >> a >> b >> c;
   int x, y, v;
   for(int i = 0; i < m; i ++) {
     cin >> x >> y >> v;
     adj[x].pb({v, y});
+    adj[y].pb({v, x});
   }
-  vector<int> p(n+1);
-  ll st = 0, en = 1e14+5;
+  ll st = 0, en = mx;
+  // { 값, 좌표}
   while(st + 1 < en) {
     ll mid = (st+en)/2;
-    vector<ll> d(n+1, mx);
-    priority_queue<pi, vector<pi>, greater<pi>> pq;
-    pq.push({0, a});
-    d[a] = 0;
-    while(!pq.empty()) {
-      auto cur = pq.top();
-      pq.pop();
-      if(d[cur.Y] != cur.X) continue; 
-      for(auto nxt : adj[cur.Y]) {
-        if(nxt.X > mid) continue;
-        int t = nxt.X + cur.X;
-        if(t < d[nxt.Y]) {
-          d[nxt.Y] = t;
-          pq.push({t, nxt.Y});
-          p[nxt.Y] = max(p[nxt.Y], nxt.X);
-        }
-      }
-    }
-      if(d[b] == mx || d[b] > c) st = mid;
-      else en = mid;
+    if(check(mid)) st = mid;
+    else en = mid;
   }
   cout << (en == mx ? -1 : en) << nl;
 }
