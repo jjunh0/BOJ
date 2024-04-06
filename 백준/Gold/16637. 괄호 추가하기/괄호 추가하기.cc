f06d6c30d7911c08c99cc35d1a98e40be49e5dc7
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-04-06 14:55:55 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int mx = 0x3f3f3f3f;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+#define int ll
+int N, ans = -1e15;
+string s;
+void func(int cur, int val) {
+  if(cur >= N) {
+    ans = max(ans, val);
+    return;
+  }
+  int tmp = val;
+  if(s[cur-1] == '+') {
+    tmp += (s[cur]-'0');
+  }
+  else if(s[cur-1] == '-') {
+    tmp -= (s[cur]-'0');
+  }
+  else if(s[cur-1] == '*') {
+    tmp *= (s[cur]-'0');
+  }
+  func(cur+2, tmp);
+  if(cur == N-1) {
+    return;
+  }
+  int b = (s[cur] - '0');
+  if(s[cur+1] == '+') {
+    b += (s[cur+2] -'0');
+  }
+  else if(s[cur+1] == '-') {
+    b -= (s[cur+2] -'0');
+  }
+  else if(s[cur+1] == '*') {
+    b *= (s[cur+2] -'0');
+  }
+  tmp = val;
+  if(s[cur-1] == '+') {
+    tmp += b;
+  }
+  else if(s[cur-1] == '-') {
+    tmp -= b;
+  }
+  else if(s[cur-1] == '*') {
+    tmp *= b;
+  }
+  func(cur+4, tmp);
+}
+
+void solve() {
+  cin >> N;
+  cin >> s;
+  func(2, s[0]-'0');
+  cout << ans;
+}
+
+// ************************************
+
+signed main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int tt = 1;
+  while(tt--) {
+    solve();
+  }
+}
