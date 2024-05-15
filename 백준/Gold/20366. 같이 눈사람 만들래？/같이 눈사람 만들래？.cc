cd4998122b14f000cb99dab9f137174d49a3432c
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-05-15 19:35:34 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 168 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int mx = 0x3f3f3f3f;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int n;
+int a[605];
+void solve() {
+  cin >> n;
+  for(int i = 0; i < n; i ++) cin >> a[i];
+  sort(a, a+n);
+  int ans = 0x7fffffff;
+  for(int i = 0; i < n; i ++) {
+    for(int j = i+3; j < n; j ++) {
+      int st = i+1, en = j-1;
+      int m = a[i] + a[j];
+      while(st < en) {
+        int s = a[st] + a[en];
+        ans = min(ans, abs(m-s));
+        if(s < m) st ++;
+        else en --;
+      }
+    }
+  }
+  cout << ans;
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    while(tt--) {
+        solve();
+    }
+}
