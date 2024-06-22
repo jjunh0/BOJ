cf8353177f7b144c9079799198e8c8a31d9ef0fb
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-06-22 23:59:15 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int mx = 0x3f3f3f3f;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solve() {
+  ll n;
+  cin >> n;
+  n = n%5;
+  if(n == 1 || n == 4 || n == 3) cout << "SK";
+  else cout << "CY";
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    while(tt--) {
+        solve();
+    }
+}
