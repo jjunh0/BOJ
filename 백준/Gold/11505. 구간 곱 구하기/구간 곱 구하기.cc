f75fad2e2baa3e7c93d31d97c5f47f3a1b5d8018
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-06-25 21:59:41 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 116 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int mx = 0x3f3f3f3f;
+#define int ll
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+int arr[2097159];
+int n, k, m, N=1;
+const int md = 1000000007;
+int sum(int L, int R, int nodeNum, int nodeL, int nodeR){
+    if(R < nodeL || nodeR < L) return 1;
+    if(L <= nodeL && nodeR <= R) return arr[nodeNum];
+    int mid = (nodeL + nodeR) / 2;
+    return sum(L, R, nodeNum*2, nodeL, mid) * sum(L, R, nodeNum*2+1, mid+1, nodeR)%md;
+}
+
+void update(int i, int val){
+    i += N;
+    arr[i] = val;
+    while(i > 1){
+        i /= 2;
+        arr[i] = arr[i*2] * arr[i*2+1] % md;
+    }
+}
+
+void construct(){
+    for(int i = N-1; i > 0; --i)
+        arr[i] = arr[i*2] * arr[i*2+1] % md; 
+}
+
+
+
+void solve() {
+  cin >> n >> k >> m;
+  while(N < n) {
+    N = (N<<1);
+  }
+  fill(arr, arr+2097159, 1);
+  for(int i = 0; i < n; i ++) cin >> arr[N+i];
+  construct();
+  for(int i = 0; i < k+m; i ++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    b --;
+    if(a == 1) {
+      update(b, c);
+    }
+    else {
+      c --;
+      cout << sum(b, c, 1, 0, N-1)%md << nl;;
+    }
+  }
+}
+
+// ************************************
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    while(tt--) {
+        solve();
+    }
+} 
