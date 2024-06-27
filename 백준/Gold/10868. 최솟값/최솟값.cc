e9f57162d23335133a7bf5ed0901d800fc6f7202
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-06-27 22:51:25 +0900

    [Gold I] Title: 최솟값, Time: 68 ms, Memory: 4076 KB -BaekjoonHub

---
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int mx = 0x3f3f3f3f;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+struct SegTree{
+	int size, N;
+	vector<ll> arr;
+	SegTree(int n){
+		size = n;
+		N = 1;
+		while(N < size) N *= 2;
+    arr.resize(2*N+1);
+    fill(all(arr), mx);
+	}
+	void update(int i, long long val){
+		i += N;
+		arr[i] = val;
+		while(i > 1){
+			i /= 2;
+			arr[i] = min(arr[i*2],arr[i*2+1]);
+		}
+	}
+  void construct(){
+		for(int i=N-1; i>0; i--)
+			arr[i] = min(arr[i*2], arr[i*2+1]);
+	}
+  void init(int i, ll val){
+		i += N;
+    arr[i] = val;
+	}
+  ll get(int L, int R, int nodeNum, int nodeL, int nodeR){
+      if(R < nodeL || nodeR < L) return mx;
+      if(L <= nodeL && nodeR <= R) return arr[nodeNum];
+      int mid = (nodeL + nodeR) / 2;
+      return min(get(L, R, nodeNum*2, nodeL, mid), get(L, R, nodeNum*2+1, mid+1, nodeR));
+  }
+  ll get(int L, int R) {
+    return get(L, R, 1, 0, N-1);
+  }
+};
+
+void solve() {
+  int N, M;
+  cin >> N >> M;
+  SegTree st(N);
+  for(int i = 0; i < N; i ++) {
+    int a;
+    cin >> a;
+    st.init(i, a);
+  }
+  st.construct();
+  while(M--) {
+    int a, b;
+    cin >> a >> b;
+    cout << st.get(a-1, b-1) << nl; 
+  }
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    while(tt--) {
+        solve();
+    }
+}
