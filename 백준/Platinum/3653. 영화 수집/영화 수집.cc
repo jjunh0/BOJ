ed9eae9d199e5363b7196f1043b55e45b677df15
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-07-02 14:04:00 +0900

    [Platinum IV] Title: 영화 수집, Time: 144 ms, Memory: 6892 KB -BaekjoonHub

---
+++ 백준/Platinum/3653. 영화 수집/영화 수집.cc
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int mx = 0x3f3f3f3f;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+struct SegTree{
+	int size, N;
+	vector<ll> arr;
+	SegTree(int n){
+		size = n;
+		N = 1;
+		while(N < size) N *= 2;
+    arr.resize(2*N+1);
+	}
+	void update(int i, ll val){
+		i += N;
+		arr[i] = val;
+		while(i > 1){
+			i /= 2;
+			arr[i] = arr[i*2] + arr[i*2+1];
+		}
+	}
+  void construct(){
+		for(int i=N-1; i>0; i--)
+			arr[i] = arr[i*2] + arr[i*2+1];
+	}
+  void init(int i, ll val){
+		i += N;
+    arr[i] = val;
+	}
+  ll get(int L, int R, int nodeNum, int nodeL, int nodeR){
+      if(R < nodeL || nodeR < L) return 0;
+      if(L <= nodeL && nodeR <= R) return arr[nodeNum];
+      int mid = (nodeL + nodeR) / 2;
+      return get(L, R, nodeNum*2, nodeL, mid) + get(L, R, nodeNum*2+1, mid+1, nodeR);
+  }
+  ll get(int L, int R) {
+    return get(L, R, 1, 0, N-1);
+  }
+};
+
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  SegTree st(n+m+5);
+  vector<int> mp(n+1);
+  int idx = 1;
+  for(int i = n; i >= 1; i --) {
+    mp[i] = idx;
+    st.init(idx, 1);
+    idx ++;
+  }
+  st.construct();
+  while(m--) {
+    int t;
+    cin >> t;
+    cout << st.get(mp[t]+1, idx) << ' ';
+    st.update(mp[t], 0);
+    mp[t] = idx++;
+    st.update(mp[t], 1);
+  }
+  cout << nl;
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        solve();
+    }
+}
