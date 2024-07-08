46b6212367012ec1f410c9f20751a42bd42c7d4f
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-07-08 23:47:13 +0900

    [Gold II] Title: 피보나치 수 3, Time: 8 ms, Memory: 7752 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int MX = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solve() {
+  ll N;
+  cin >> N;
+  int n = N%1500000;
+  int f[n+1];
+  f[1] = 1; f[2] = 1;
+  for(int i = 3; i <= n; i ++) {
+    f[i] = (f[i-1] + f[i-2])%1000000;
+  }
+  cout << f[n];
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    while(tt--) {
+        solve();
+    }
+}
