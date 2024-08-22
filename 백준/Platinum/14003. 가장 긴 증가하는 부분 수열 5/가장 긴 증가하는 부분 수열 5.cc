8740bdd7d8e11e49c5335784977963b0290748ee
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-08-23 00:05:44 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 176 ms, Memory: 22252 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -25,32 +25,29 @@ int dy[] = {0, 1, 0, -1};
 void solve() {
   int n;
   cin >> n;
-  vector<int> A(n), lis, idx, pre(n), ans;
+  vector<int> A(n), lis, dp(n), ans;
 
   for(auto &e : A) cin >> e;
   for(int i = 0; i < n; i ++) {
     if(lis.empty() || lis.back() < A[i]) {
-      if(!lis.empty()) pre[i] = idx.back();
-      else pre[i] = -1;
       lis.pb(A[i]);
-      idx.pb(i);
+      dp[i] = sz(lis);
     }
     else {
       auto it = lower_bound(all(lis), A[i]) - lis.begin();
       lis[it] = A[i];
-      idx[it] = i;
-      if(it == 0) pre[i] = -1;
-      else pre[i] = idx[it-1];
+      dp[i] = it+1;
     }
-
   }
-  int cur = idx[sz(lis)-1];
-  while(cur != -1) {
-    ans.pb(A[cur]);
-    cur = pre[cur];
+  int cur = sz(lis);
+  for(int i = n-1; i >= 0; i--) {
+    if(dp[i] == cur) {
+      ans.pb(A[i]);
+      cur --;
+    }
   }
-  reverse(all(ans));
   cout << sz(ans) << nl;
+  reverse(all(ans));
   for(auto e : ans) cout << e << ' ';
 }
 
