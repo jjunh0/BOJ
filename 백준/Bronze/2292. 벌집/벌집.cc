6bbca923b1bbb7da299f9d6ae2ba71ea1956b585
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-08-23 18:24:01 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int MX = 0x3f3f3f3f;
+const int MOD = 1'000'000'007;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solve() {
+  int n;
+  cin >> n;
+  int cur = 1, i;
+  for(i = 1; cur < n; i ++) {
+    cur += 6*i;
+  }
+  cout << i;
+
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1;
+    while(tt--) {
+        solve();
+    }
+}
