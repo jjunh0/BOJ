0f5f9ec03bb31eff87d4a0d8b1fab3d715b5a817
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-08-30 00:18:45 +0900

    [Silver IV] Title: solved.ac, Time: 28 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef pair<ll,ll> pl;
+
+const char nl = '\n';
+const int MX = 0x3f3f3f3f;
+const int MOD = (int)1e9 + 7;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> A(n);
+  for(auto &e: A) cin >> e;
+  sort(all(A));
+  int l = lround(0.15 * n);
+  cout << (n != 0 ?lround(1. *accumulate(A.begin() + l, A.begin() + n-l, 0) / (n-2*l)) : 0);
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tests = 1;
+    while(tests--) {
+        solve();
+    }
+}
