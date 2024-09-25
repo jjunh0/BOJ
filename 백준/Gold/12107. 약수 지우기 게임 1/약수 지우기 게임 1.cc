99d306c1493fcccf878363f4f423205fe33a684c
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-09-25 21:20:39 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef pair<ll,ll> pl;
+
+const char nl = '\n';
+const int MX = 0x3f3f3f3f;
+const int MOD = (int)1e9 + 7;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solve() {
+  int N;
+  cin >> N;
+  cout << (N==1 ? "B" : "A");
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tests = 1;
+    while(tests--) {
+        solve();
+    }
+}
