62cbe4d6eaa2a41fffe9e6c430722e5decd58094
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-10-02 19:00:19 +0900

    [Gold I] Title: 소풍, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1242. 소풍/소풍.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef pair<ll,ll> pl;
+
+const char nl = '\n';
+const int MX = 0x3f3f3f3f;
+const int MOD = (int)1e9 + 7;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void solve() {
+  int n, K, m, ans = 0;
+  cin >> n >> K >> m;
+  
+  while(n > 0) {
+    int k = K%(n);
+    if(k == 0) k = n;
+    ans ++;
+    if(m == k) {
+      break;
+    }
+    if(m > k) m = m - k;
+    else m = n-k+m;
+    n --;
+  }
+  cout << ans;
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tests = 1;
+    while(tests--) {
+        solve();
+    }
+}
