846807ed84408e71c33ed14105d7b31fd4029a74
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2025-01-11 18:18:09 +0900

    [Gold III] Title: 고기잡이, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7573. 고기잡이/고기잡이.cc
+++ 백준/Gold/7573. 고기잡이/고기잡이.cc
@@ -18,29 +18,32 @@ const int MOD = (int)1e9 + 7;
 
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
+vector<pi> cand(105);
+int N, I, M;
+
+int f(int sx, int sy, int xx, int yy) {
+  int ret = 0;
+  for(int i = 0; i < M; i ++) {
+    if(cand[i].X >= sx && cand[i].X <= sx + xx &&
+    cand[i].Y >= sy && cand[i].Y <= sy + yy) ret ++;
+  }
+  return ret;
+}
+
 void solve() {
-  int N, I, M;
   cin >> N >> I >> M;
-  vector<pi> cand(M);
   for(int i = 0; i < M; i ++) {
     int x, y;
     cin >> x >> y;
     cand[i] = {x, y};
   }
   int ans = 0;
-  sort(all(cand));
   for(int i = 0; i < M; i ++) {
-    for(int j = 0; j < M; j ++) {
-      int rec_x = cand[i].X;
-      int rec_y = cand[j].Y;
-      for(int ri = 1; ri < I/2; ri ++) {
-        int rj = I/2 - ri;
-        int cnt = 0; 
-        for(int k = 0; k < M; k ++) {
-          if(cand[k].X >= rec_x && cand[k].X <= rec_x + ri &&
-            cand[k].Y >= rec_y && cand[k].Y <= rec_y + rj) cnt ++;
-        }
-        ans = max(ans, cnt);
+    for(int xx = 1; xx < I/2; xx ++) {
+      int yy = I/2 - xx;
+      for(int xxx = max(cand[i].X - xx, 1); xxx <= cand[i].X; xxx ++) {
+        int ret = f(xxx, cand[i].Y, xx, yy);
+        ans = max(ret, ans);
       }
     }
   }
