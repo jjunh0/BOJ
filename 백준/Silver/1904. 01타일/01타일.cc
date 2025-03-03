011506584a33d942543b37b301335aa074164de2
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2025-03-03 23:15:45 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define ll long long
+#define X first
+#define Y second
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+typedef pair<ll,ll> pl;
+
+const char nl = '\n';
+const int MX = 0x3f3f3f3f;
+const int MOD = (int)1e9 + 7;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+int dp[1000005];
+void solve() {
+  int N;
+  cin >> N;
+  dp[1] = 1;
+  dp[2] = 2;
+  for(int i = 3; i <= N; i ++) {
+    dp[i] = (dp[i-1] + dp[i-2])%15746;
+  }
+  cout << dp[N];
+}
+
+// ************************************
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tests = 1;
+    while(tests--) {
+        solve();
+    }
+}
